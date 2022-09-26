a44baf76dac5e18a781b31b3756f9b8bcb24a66d
TereshchenkoVS <110491877+TereshchenkoVS@users.noreply.github.com>
2022-09-26 18:01:31 +0300

    Completed Ex 004

---
+++ Homework02/Ex004/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход семь чисел, и находит их среднее арифметическое
+
+System.Console.WriteLine(" Введите 7 чисел:");
+Console.WriteLine("Число a = ");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число b = ");
+double b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число c = ");
+double c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число d = ");
+double d = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число e = ");
+double e = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число f = ");
+double f = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число g = ");
+double g = Convert.ToInt32(Console.ReadLine());
+
+double rezult = (a + b + c + d + e + f + g) / 2;
+
+Console.WriteLine(rezult);
